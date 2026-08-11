84aec63d4d77cd3a2c30d9c928dfe38027e42e4e
WorkBuddy <workbuddy@local>
2026-08-11 18:41:01 +0800

    test: 补回 services.dart import 以定义 ServicesBinding
    - 上一轮误删 package:flutter/services.dart 的 import（当时以为仅 MethodCall 需要），
      但 ServicesBinding 同样定义在该包，导致 Undefined name 'ServicesBinding' 的 analyze error
    - 重新 import 后 ServicesBinding 仍被使用，不会触发 unused_import

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,5 +1,6 @@
 import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:hive/hive.dart';
